eb9f10c8350bf675bf9fa568e6ee77c871a69679
Oh Dylan <ddragon1128@hotmail.com>
2021-10-14 22:20:40 +0800

    Added the functionality to add item

--- script.js
+++ script.js
@@ -93,6 +93,30 @@ function updateDOM() {
 
 }
 
+// Add to column list, reset textbox
+function addToColumn(column){
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = '';
+  updateDOM()
+}
+
+// Show Add Item Input Box
+function showInputBox(column){
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+}
+
+// Hide Item input box
+function hideInputBox(column){
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+}
+
 //  Allow arrays to reflect drag and drop items
 function rebuildArrays(){
   backlogListArray = [];
